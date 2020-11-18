f174e6eb4c42bb55e3f999422f8b1a313f63f045
superjuho <puurunen.tuho@gmail.com>
2020-11-18 20:58:54 +0200

    mihinköhän tää menee :--D

---
+++ extension/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function (nodecg) {
+	nodecg.Replicant('textReplicant', {defaultValue: `Hello, I'm one of the graphics in your bundle! I'm where you put the graphics you want to run in your broadcast software!
+`});
+};
